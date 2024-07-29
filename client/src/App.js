2605fdd793527c55436631b4f505715170dbd4d9
= <=>
2024-07-29 22:30:27 +1200

    Adding boilerplate - may change to Vue

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p id={styles.demoText}>Web2Life</p>
+    </div>
+  );
+}
+
+export default App;
