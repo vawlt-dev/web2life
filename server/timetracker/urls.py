bfab2de088e0a445540e96fa8fce91c930234051
Jamie Dennis <jdennis9@proton.me>
2024-08-01 21:29:54 +1200

    Serve React output through Django

--- server/timetracker/urls.py
+++ server/timetracker/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
+from . import views
+from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("timetracker.urls")),
+    re_path(r"^(?P<path>.*)$", views.serve_react, {"document_root": settings.FRONTEND_BUILD_PATH}),
 ]
---
+++ server/timetracker/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils._os import safe_join
+import posixpath
+from pathlib import Path
+from django.views.static import serve
+
+def serve_react(request, path, document_root=None):
+    path = posixpath.normpath(path).lstrip('/')
+    if Path(safe_join(document_root, path)).is_file():
+        return serve(request, path, document_root)
+    else: return serve(request, "index.html", document_root)
