4dd33e81519c8b3776bcc2b1bcc6213eeef85f7e
= <=>
2024-08-03 00:09:35 +1200

    server now runs app

--- server/timetracker/urls.py
+++ server/timetracker/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, re_path
 from . import views
 from django.conf import settings
 
 urlpatterns = [
-    re_path(r"^(?P<path>.*)$", views.serve_react, {"document_root": settings.FRONTEND_BUILD_PATH}),
+    re_path(
+        r"^(?P<path>.*)$",
+        views.serve_react,
+        {"document_root": settings.FRONTEND_BUILD_PATH},
+    ),
 ]
--- server/web2life/settings.py
+++ server/web2life/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -23,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3n0vpnp8gxqz=gg3l(_mv6cd&rt^0olyzbk*cte$9szi(^y96*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
