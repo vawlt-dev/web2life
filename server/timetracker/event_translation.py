a5598ebd46f33524d9014a05a363a8fe4c72d2de
Jamie Dennis <jdennis9@proton.me>
2024-09-10 15:41:44 +1200

    Fix github events having incorrect timestamps

--- server/timetracker/event_translation.py
+++ server/timetracker/event_translation.py
@@ -12,7 +12,8 @@ def generate_project_name_from_github_repo(repo):
 
 # @NOTE(Jamie D)
 # Returns an array of Event objects. Commits that happen within the same 1-hour
-# time frame are grouped into one event. Data should be an array of events that came from
+# time frame are grouped into one event called something like "Pushed <no. commits> commits to <respository>".
+# Data should be an array of events that came from
 # Github. @TODO: Only works on PushEvent for now
 def translate_github_events(data) -> list:
     # repository name -> (hour -> commit count) >:(
@@ -20,7 +21,7 @@ def translate_github_events(data) -> list:
 
     for e in data:
         time = datetime.strptime(e["time"], "%Y-%m-%dT%H:%M:%SZ")
-        ts = calendar.timegm(time.timetuple())
+        ts = time.timestamp()
         hour = int(ts / 3600)
         event_map[e["repo"]][hour] += 1
 
@@ -33,19 +34,19 @@ def translate_github_events(data) -> list:
             e = Events(
                 title = f"Pushed {count} commits to {repo}",
                 projId = models.get_or_add_project_from_name(repo),
-                start = datetime.fromtimestamp(hour),
-                end = datetime.fromtimestamp(hour + 1),
+                start = datetime.fromtimestamp(hour * 3600),
+                end = datetime.fromtimestamp((hour + 1) * 3600),
                 allDay = False,
                 task = "GitHub commit",
                 description = "",
             )
 
+            print("From ", e.start)
+            print("To ", e.end)
+
             events.append(e)
 
     return events
-
-
-    
 
 #@TODO: Set up a POST to use this
 def map_slack_channel_to_project(channel_name, project_name):
